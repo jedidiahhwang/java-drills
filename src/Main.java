2e9fbcb007154b85590d9154b1fc2b51fee25e66
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-05-28 20:49:04 -0600

    wrapping up step 1 and step 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+        String firstName = "Qui-Gon";
+        String lastName = "Jinn";
+        String bornYear = "80 BBY";
+        String homeworld = "Coruscant";
+        double height = 1.93 * 3.28084;
+        double weight = 200;
 
+        double purse = 150.0;
+        purse = purse - 24.3;
+        purse = purse - 45;
+        purse = purse * 2;
+        purse = purse - (purse * .1);
+        System.out.println(purse);
     }
 }
